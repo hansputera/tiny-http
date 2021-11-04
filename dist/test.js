8e30eecac68b3d50e40487a2fc8fe316689b56ae
hansputera <hanifdwyputrasembiring@gmail.com>
2021-11-04 03:48:07 +0000

    rem(file): remove test file

--- dist/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-_1.tinyHttp.get('https://raznar.id').then((resp) => {
-    console.log(resp.getContent());
-});
--- lib/test.ts
+++
@@ -1,5 +0,0 @@
-import { tinyHttp } from '.';
-
-tinyHttp.get('https://raznar.id').then((resp) => {
-    console.log(resp.getContent());
-});
